2690774cf28a6ecd63e7081ceee6d10a30aa9597
Kevin <hellokevinvogel@gmail.com>
2022-03-19 20:04:20 +0100

    chore(): add outputs and vars

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
+}
+
+variable "access_ip" {
+  type = string
+}
+
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
