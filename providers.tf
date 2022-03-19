448515e28606b9ce6f4ae60ff7d927e6dafe728b
Kevin <hellokevinvogel@gmail.com>
2022-03-19 20:04:08 +0100

    chore(): add provider

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
