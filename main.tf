05741f9a17d66bff31384dfa591f284ae11a6ea0
Kevin <hellokevinvogel@gmail.com>
2022-03-20 06:28:34 +0100

    chore(vpc): rm public_cidrs list

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "network" {
   vpc_cidr        = local.vpc_cidr
   access_ip       = var.access_ip
   security_groups = local.security_groups
-  public_cidrs    = [for i in range(2, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
 }
 
 module "ec2" {
--- network/variables.tf
+++ network/variables.tf
@@ -1,12 +1,6 @@
-# --- networking/variables.tf
-
 variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
-}
-
-variable "public_cidrs" {
-  type = list(any)
 }
 
 variable "access_ip" {
