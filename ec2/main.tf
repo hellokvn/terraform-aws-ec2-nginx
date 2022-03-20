b59a98e8fd419d474a6c66979fd06ac508c8fe8f
Kevin <hellokevinvogel@gmail.com>
2022-03-20 07:27:28 +0100

    chore(ec2): set instance_type to t2.micro

--- ec2/main.tf
+++ ec2/main.tf
@@ -16,11 +16,11 @@ resource "aws_key_pair" "key_pair" {
 
 # Create a EC2 Instance (Ubuntu 20)
 resource "aws_instance" "node" {
-  instance_type           = "t2.small"
-  ami                     = "ami-0d527b8c289b4af7f"
-  key_name                = aws_key_pair.key_pair.id
-  vpc_security_group_ids  = [var.public_sg]
-  subnet_id               = var.public_subnet
+  instance_type          = "t2.micro" # free instance
+  ami                    = "ami-0d527b8c289b4af7f"
+  key_name               = aws_key_pair.key_pair.id
+  vpc_security_group_ids = [var.public_sg]
+  subnet_id              = var.public_subnet
 
   tags = {
     Name = "TF Generated EC2"
